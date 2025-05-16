4ea7c044881226d25af3de224dfcb98e127953ce
Gayathri G <gayathrigcse2022@gmail.com>
2025-05-16 21:31:30 +0530

    .

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+    res.status(200).json({message: 'Hello world'})
+});
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
