be432b22010d10be2c69357bd5e322b1e18cc63f
Gayathri G <gayathrigcse2022@gmail.com>
2025-05-19 19:22:13 +0530

    m

---
+++ server/routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const router = express.Router();
+
+// GET /Todos
+router.get('/Todos', (req, res) => {
+    res.status(200).json({msg : "GET REQUEST TO /api/todos"})
+});
+
+// POsT /Todos
+router.post('/todos', (req, res) => {
+    res.status(201).json({msg : "POST REQUEST TO /api/todos"})
+});
+
+//DELETE /Todos/:id
+router.delete('/todos/:id', (req, res) => {
+    res.status(200).json({msg : "DELETE REQUEST TO /api/todos"})
+});
+
+// PUT /Todos/:id
+router.put('/todos/:id', (req, res) => {
+    res.status(200).json({msg : "PUT REQUEST TO /api/todos"})
+});
+
+module.exports = router;
+
+
