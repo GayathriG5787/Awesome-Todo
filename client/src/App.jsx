4a669fef14677d3209a7507de306725bb7582caf
Gayathri G <gayathrigcse2022@gmail.com>
2025-06-03 11:51:51 +0530

    com1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,17 @@
-
+import { useEffect } from "react";
 
 export default function App() {
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await fetch("/api/todos");
+      const todos = await res.json();
+      
+      console.log(todos);
+    }
+    getTodos();
+  }, [])
+
   return (
     <main className="container"> 
       <h1>Awesome Todos</h1>
--- server/routes.js
+++ server/routes.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 // GET /Todos
-router.get('/Todos', (req, res) => {
-    res.status(200).json({msg : "GET REQUEST TO /api/todos"})
+router.get('/todos', (req, res) => {
+    res.status(200).json({msg : "GET REQUEST TO /api/todos"});
 });
 
 // POsT /Todos
 router.post('/todos', (req, res) => {
-    res.status(201).json({msg : "POST REQUEST TO /api/todos"})
+    res.status(201).json({msg : "POST REQUEST TO /api/todos"});
 });
 
 //DELETE /Todos/:id
