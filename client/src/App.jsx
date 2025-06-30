edd873c2c30180af317797d297d2c1e93512141b
Gayathri G <gayathrigcse2022@gmail.com>
2025-06-30 18:16:33 +0530

    frontend communication with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
+
+  const [message, setMessage] = useState(""); 
 
   useEffect(() => {
     async function getTodos() {
       const res = await fetch("/api/todos");
       const todos = await res.json();
       
-      console.log(todos);
+      setMessage(todos.msg);
     }
     getTodos();
   }, [])
@@ -15,6 +17,7 @@ export default function App() {
   return (
     <main className="container"> 
       <h1>Awesome Todos</h1>
+      <p>{message}</p>
     </main>
   );
 }
